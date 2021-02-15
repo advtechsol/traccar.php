ae2156230af9ada05c4f68b5954baefbd238389d
advtechsol <62219715+advtechsol@users.noreply.github.com>
2021-02-15 19:23:48 +1100

    Create RockBLOCK.php

---
+++ RockBLOCK.php
@@ -0,0 +1,36 @@
+<?php
+
+$json = file_get_contents('php://input');
+
+$data = json_decode($json, true);
+
+$id=$data["imei"];
+
+$hexvalue=$data["data"];
+
+$hexdata=hex2bin($hexvalue);
+
+//echo $hexdata.'<br>' ;
+
+$items = explode(",", $hexdata); //carve up the csv string
+
+$time = $items[0];
+$lat = $items[1];
+$lng = $items[2];
+$alt = $items[3];
+$course = $items[5];
+$bat = $items[7];
+
+/*
+echo "Time: ".$time.'<br>';
+echo "Latitude: ".$lat.'<br>';
+echo "Longitude: ".$lng.'<br>';
+echo "Altitude: ".$alt.'<br>';
+echo "Course: ".$course.'<br>';
+echo "Battery: ".$bat.'<br>';
+*/
+
+$page = file_get_contents("http://iotnetwork.com.au:5055/?id=$id&lat=$lat&lon=$lng&course=$course&battery=$bat");
+
+
+?>
